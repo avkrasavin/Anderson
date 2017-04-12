788389043d41ce23f74d01dcd69699dbd81d7b13
Yaroslav <yaros72@live.com>
2017-04-12 10:56:51 +0300

    Add Lattice-Site-Basis coding + example
    Sites.py - coding lattice-site
    Basis.py - get lattice make basis

---
+++ basis.py
@@ -0,0 +1,35 @@
+import numpy as np
+import itertools
+from math import factorial
+
+class Fermion_Bose_basis(object):
+    def __init__(self, lat):
+        
+        self.size = (factorial(lat.n_fermion)/factorial(lat.n_fermion-lat.n_spin)/factorial(lat.n_spin))**2*lat.n_boson**(lat.n_boson_max+1)
+            
+        print 'basis size = ',self.size
+            
+        self.spin_up = np.empty((self.size,lat.n_fermion))
+        self.spin_down=np.empty((self.size,lat.n_fermion))
+        self.boson = np.empty((self.size,lat.n_boson))
+
+        spin_basis=[]
+        for state in itertools.product([0, 1], repeat=lat.n_fermion):
+            if np.sum(state)==lat.n_spin:
+                spin_basis.append(state)
+        spin_basis=np.array(spin_basis)
+
+        boson_basis=[]
+        for state in itertools.product(range(lat.n_boson_max+1),repeat=lat.n_boson):
+            boson_basis.append(state)
+        boson_basis=np.array(boson_basis)
+
+        L=spin_basis.shape[0];M=boson_basis.shape[0]
+        print L,M
+        for i in xrange(self.size):
+            self.spin_up[i,:] = spin_basis[i%L,:]
+            self.spin_down[i,:]=spin_basis[i//L%L,:]
+            self.boson[i,:]=boson_basis[i//(M*L),:]
+            # print self.spin_up[i,:],self.spin_down[i,:],self.boson[i,:]
+
+        self.full=np.hstack((self.spin_up,self.spin_down,self.boson))
--- sites.py
+++ sites.py
@@ -20,23 +20,20 @@ class site(object):
     def scatering_parametrs(self,gamma):
         self.gamma=gamma
         
-    def init_basis(self):
-        if self.type=='boson':
-            self.states=range(0,self.n_boson_max)
-        if self.type=='fermion':
-            self.states=range(0,4)
-        
     def __repr__(self):
         return "%s %s \n" % (self.type,self.number)
 
 class System:  
-    def __init__(self, lattice_type ,n_fermion, n_boson,n_boson_max=0):
+    def __init__(self, lattice_type ,n_fermion, n_boson,n_boson_max=0,n_spin=0):
         self.lattice_type=lattice_type
-        self.sites=[];self.L=n_fermion+n_boson
+        self.sites=[]
+        self.n_fermion=n_fermion
+        self.n_boson=n_boson
+        self.L=n_fermion+n_boson
+        self.n_boson_max=n_boson_max
+        self.n_spin = n_spin
         for i in range(n_fermion):
             self.sites.append(site(i,'fermion'))
         if n_boson>0:
             for j in range(n_boson):
                 self.sites.append(site(j+n_fermion,'boson',n_boson_max))
-        for i in range(self.L):
-            self.sites[i].init_basis()
